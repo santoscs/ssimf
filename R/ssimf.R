ec82b333ea5450d858bf5b3f652564b89cfd741f
cristiano1br <cristiano1br@gmail.com>
2016-09-05 19:21:53 -0300

    add functions

--- R/ssimf.R
+++ R/ssimf.R
@@ -1,37 +1,72 @@
 # Project: ssimf
 
-#' Ensemble Empirical Mode Decomposition
-#' 
-#' This is an EMD/EEMD program
+#' Statistic
 #'
-#' @param Y Inputted data;1-d data only
-#' @param Nstd ratio of the standard deviation of the added noise and that of Y;
-#' @param NE Ensemble number for the EEMD
-#' 
-#' @return A matrix of N*(m+1) matrix, where N is the length of the input
-#'         data Y, and m=fix(log2(N))-1. Column 1 is the original data, columns 2, 3, ...
-#'         m are the IMFs from high to low frequency, and comlumn (m+1) is the
-#'         residual (over all trend).
+#' that is used to obtain the "percenta" line based on Wu and Huang (2004).
 #'
-#' @details It should be noted that when Nstd is set to zero and NE is set to 1, the
-#' program degenerates to a EMD program.(for EMD Nstd=0,NE=1)
-#' This code limited sift number=10 ,the stoppage criteria can't change. 
+#' @details For this program to work well, the minimum data length is 36
+#'
+#' @param percenta: a parameter having a value between 0.0 ~ 1.0, e.g., 0.05 
+#'                 represents 95#' confidence level (upper bound); and 0.95 
+#'                 represents 5#' confidence level (lower bound)
+#' @param imfs: the true IMFs from running EMD code. The first IMF must
+#'                 be included for it is used to obtain the relative mean
+#'                 energy for other IMFs. The trend is not included.
+#'
+#' @return \item{sigline}{a two column matrix, with the first column the natural
+#'                 logarithm of mean period, and the second column the
+#'                 natural logarithm of mean energy for significance line}
+#'          \item{logep}{a two colum matrix, with the first column the natural
+#'                 logarithm of mean period, and the second column the
+#'                 natural logarithm of mean energy for all IMFs}
 #'
 #' @references Wu, Z., and N. E Huang (2008). Ensemble Empirical Mode Decomposition: a noise-assisted data analysis method.
 #' Advances in Adaptive Data Analysis. Vol.1, No.1. 1-41.  
 #' 
-#' @import  RcppOctave
+#' @import RcppOctave
 #' 
 #' @export
+#'
 
-eemd <- function(Y, Nstd, NE){
+statistic <- function(imfs){
   o_clear(all=TRUE)
-  o_source("octave.m")
-  o_assign(Y=Y)
-  o_assign(Nstd=Nstd)
-  o_assign(NE=NE)
-  ans <- o_eval("eemd(Y,Nstd,NE)")
-  return(ans)
+  o_source("inst/octave.m")
+  o_assign(warning('off', 'Octave:possible-matlab-short-circuit-operator');)
+  o_assign(imfs=imfs)
+  logep <- o_eval("statistic(imfs)")
+  return(logep)
 }
 
+#' Critical Value
+#'
+#' that is used to obtain the "percenta" line based on Wu and
+#' Huang (2004).
+#'
+#' @details For this program to work well, the minimum data length is 36
+#'
+#'  
+#' @param percenta a parameter having a value between 0.0 ~ 1.0, e.g., 0.05 
+#' represents 95#' confidence level (upper bound); and 0.95 
+#' represents 5#' confidence level (lower bound) 
+#' @param imfs the true IMFs from running EMD code. The first IMF must
+#' be included for it is used to obtain the relative mean
+#' energy for other IMFs. The trend is not included.
+#'   
+#' @return sigline: a two column matrix, with the first column the natural
+#' logarithm of mean period, and the second column the
+#' natural logarithm of mean energy for significance line
+#' 
+#' @references Wu, Z., and N. E Huang (2008). Ensemble Empirical Mode Decomposition: a noise-assisted data analysis method.
+#' Advances in Adaptive Data Analysis. Vol.1, No.1. 1-41.
+#'
+#'
+
+criticalvalue <- function(n, percenta){
+  o_clear(all=TRUE)
+  o_source("inst/octave.m")
+  o_assign(warning('off', 'Octave:possible-matlab-short-circuit-operator');)
+  o_assign(imfs=imfs)
+  logep <- o_eval("statistic(imfs)")
+  return(logep)
+}
 
